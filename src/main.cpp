3a43080391ed789245ea169d344cad4a345d2548
ugur <55594186+ugururesin@users.noreply.github.com>
2020-08-30 19:34:00 +0300

    Define functions

--- src/main.cpp
+++ src/main.cpp
@@ -38,6 +38,17 @@ int main() {
    * TODO: Initialize the pid variable.
    */
 
+  /* EXPLORATION of DIFFERENT CONTROLLERS
+  (Parameters are selected from the course, initially)
+  */
+  // P-Controller
+  pid.Init(0.05,0.0,0.0)
+  // PD-Controller
+  pid.Init(0.05,0.0,3.0)
+  // PID-Controller
+  pid.Init(0.05,0.04,0.0)
+
+
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, 
                      uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -63,6 +74,8 @@ int main() {
            * NOTE: Feel free to play around with the throttle and speed.
            *   Maybe use another PID controller to control the speed!
            */
+          pid.UpdateError(cte);
+          steer_value -= pid.TotalError();
           
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value 
